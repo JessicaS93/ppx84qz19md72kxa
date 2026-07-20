ed8f2867da5319bfb98f34712268934e003bfeb1
JessicaS93 <shbpayer@gmail.com>
2026-07-20 07:50:16 -0600

    Actualizar v3.3

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='proyeccion-v32-diagnostico';
+const CACHE='proyeccion-v33-imagen-fix';
 const ASSETS=['./','./index.html','./diagnostico.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
